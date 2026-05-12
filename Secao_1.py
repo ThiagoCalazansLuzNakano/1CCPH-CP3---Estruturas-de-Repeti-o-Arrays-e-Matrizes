c85ddabcae650c1ce3e7a853909feb58202a448d
Thiago Nakano <tclnakano@gmail.com>
2026-05-12 09:55:39 -0300

    CP3

--- Secao_1.py
+++ Secao_1.py
@@ -3,22 +3,24 @@ temperaturas_sala = [[28, 31, 34, 33],[25, 27, 29, 28],[32, 35, 36, 34],[24, 26,
 for sala in temperaturas_sala:
     soma = 0
     critico = 0
-    Sala =0
     for temperatura in sala:
         soma = soma + temperatura
         if temperatura >= 33:
             critico += 1
 
-    print(f"Sala {Sala + 1}")
+    print(f"Sala")
     print(soma / 4)
     print(critico)
-    print(f"Sala {Sala + 2}")
+    print()
+    print(f"Sala")
     print(soma / 4)
     print(critico)
-    print(f"Sala {Sala +3}")
+    print()
+    print(f"Sala")
     print(soma / 4)
     print(critico)
-    print(f"Sala {Sala +4}")
+    print()
+    print(f"Sala")
     print(soma / 4)
     print(critico)
 
